d39326ff6dbf113e51d8b92e1de147519a42e4c9
Oleg Tolmatcev <oleg.tolmatcev@yahoo.de>
2014-04-12 16:25:03 +0200

    add GPL3+ header to all Java files

--- src/main/java/jmdicttodsl/Header.java
+++ src/main/java/jmdicttodsl/Header.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Oleg Tolmatcev <oleg.tolmatcev@yahoo.de>
+ * Copyright (C) 2011-2014 Oleg Tolmatcev <oleg.tolmatcev@yahoo.de>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package jmdicttodsl;
 
 /**
